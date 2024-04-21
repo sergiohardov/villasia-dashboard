8cbd562fa1dc63f2c357ede8f66bc99959c64d20
Sergio Hardov <59309576+sergiohardov@users.noreply.github.com>
2024-04-22 02:22:49 +0300

    Install MUI (#3)
    * Install MUI
    
    * Add base styles & fonts

--- dev/src/main.jsx
+++ dev/src/main.jsx
@@ -1,10 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
+import { CssBaseline } from "@mui/material";
 import App from "./App.jsx";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <CssBaseline />
     <App />
   </Provider>
 );
