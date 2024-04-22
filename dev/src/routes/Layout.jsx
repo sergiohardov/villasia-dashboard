d5b75c13b050afc813cc17d248fc725332f32977
sergiohardov <sergiohardov@gmail.com>
2024-04-22 11:25:14 +0300

    Create header

--- dev/src/routes/Layout.jsx
+++ dev/src/routes/Layout.jsx
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { Box, Toolbar } from "@mui/material";
+
+import Header from "../components/Header";
 
 export default function Layout() {
   return (
-    <div>
+    <Box sx={{ display: "flex" }}>
+      <Header />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
       <Outlet />
-    </div>
+      </Box>
+    </Box>
   );
 }
